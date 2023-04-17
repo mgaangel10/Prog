a46e6ed5ef3bcffaa7faa386e5ca0a986f5fa9ad
mgaangel10 <mg.aangel10@gmail.com>
2023-04-18 00:27:18 +0200

    clase scanner

---
+++ EjerciciosExcepcionesCasa/src/ejercicio1/Main.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Indique su codigo de trabajador");
+		String codigoTrabajador = scan.nextLine();
+		System.out.println("su codigo es este: "+codigoTrabajador);
+		 
+	
+		
+		
+	}
+
+}
